81711f5fc0fe9e61e61ba944dd6857bf883bd3f4
vperezcerv <vperezcerveleon@gmail.com>
2023-11-24 16:42:45 -0500

    RECONFIGURACION JOBS TERRAFOMR

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"  # Cambia según tu región
+  region = "us-east-1" # Cambia según tu región
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = "bucket-deploys3-angular-vapc-1098824"  # Cambia a un nombre único
+  bucket = "bucket-deploys3-angular-vapc-1098824" # Cambia a un nombre único
   acl    = "public-read"
 
 }
