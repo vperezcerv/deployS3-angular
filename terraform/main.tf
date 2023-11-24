8b2fb4e76f14b803c8ea911d44384a531206cfd8
vperezcerv <vperezcerveleon@gmail.com>
2023-11-24 15:39:03 -0500

    actualización configuración terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,25 @@ resource "aws_s3_bucket" "static_site" {
   bucket = "bucket-deploys3-angular-vapc-1098824"  # Cambia a un nombre único
   acl    = "public-read"
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+}
+
+resource "aws_s3_bucket_website_configuration" "configuration_web" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
   }
 }
