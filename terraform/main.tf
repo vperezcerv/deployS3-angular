8b22134f2f468dcd1630ca3dc90a6e69e3d66b65
vperezcerv <vperezcerveleon@gmail.com>
2023-11-24 16:52:44 -0500

    Se agregan variables de entorno al pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1" # Cambia según tu región
-}
-
 resource "aws_s3_bucket" "static_site" {
   bucket = "bucket-deploys3-angular-vapc-1098824" # Cambia a un nombre único
   acl    = "public-read"
