25d36a00e5e0a60372a405304e4f34c18614abf3
vperezcerv <vperezcerveleon@gmail.com>
2023-11-24 12:27:40 -0500

    se cambian los archivos de ruta

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+
+}
