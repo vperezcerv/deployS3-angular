a2fb789b9f7b9737bf50cd2f841ce335e7abc979
vperezcerv <vperezcerveleon@gmail.com>
2023-11-28 10:54:37 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "static_site" {
   bucket = "bucket-deploys3-angular-vapc-1098824"  # Reemplaza con un nombre único
 
   acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
 
 resource "aws_s3_bucket_object" "web_files" {
@@ -10,11 +15,4 @@ resource "aws_s3_bucket_object" "web_files" {
   bucket = aws_s3_bucket.static_site.bucket
   key    = each.value
   source = "${path.module}/../../dist/${each.value}"
-}
-
-resource "aws_s3_bucket_website" "static_site" {
-  bucket = aws_s3_bucket.static_site.bucket
-
-  index_document = "index.html"
-  error_document = "error.html"
 }
