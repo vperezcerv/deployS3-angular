79c1d5927255c1fdfda099e33da468deab45503d
vperezcerv <vperezcerveleon@gmail.com>
2023-11-28 10:37:48 -0500

    se actualizan los archivos de terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,18 @@
 resource "aws_s3_bucket" "static_site" {
-  bucket = "bucket-deploys3-angular-vapc-1098824" # Cambia a un nombre único
+  bucket = "bucket-deploys3-angular-vapc-1098824"  # Reemplaza con un nombre único
+
   acl    = "public-read"
 
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
 
-resource "aws_s3_bucket_website_configuration" "configuration_web" {
-  bucket = aws_s3_bucket.static_site.id
+resource "aws_s3_bucket_object" "web_files" {
+  for_each = fileset("${path.module}/../../dist", "**/*.*")
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
+  bucket = aws_s3_bucket.static_site.bucket
+  key    = each.value
+  source = "${path.module}/../../dist/${each.value}"
 }
