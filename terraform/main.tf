9b8e7541fbdd2d812e0e4c785cff1aa38b28c90b
vperezcerv <vperezcerveleon@gmail.com>
2023-11-27 20:37:53 -0500

    se cambian resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,3 @@ resource "aws_s3_bucket" "static_site" {
   acl    = "public-read"
 
 }
-
-resource "aws_s3_bucket_website_configuration" "configuration_web" {
-  bucket = aws_s3_bucket.static_site.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
-}
